1235789369c0ef057e422acd27f3bacb6720d9b5
Rebeca <rebecavelez@gmail.com>
2023-05-19 17:10:19 +0200

    v 0.0.4 Creados los test sin errores

---
+++ CalculadoraPrueba/src/test/java/com/prueba/calculadora/tests.java
@@ -0,0 +1,53 @@
+
+/** @author Rebeca Velez*/
+
+
+package com.prueba.calculadora;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.prueba.calculadora.servicios.CalculadoraService;
+
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class tests {
+	
+	@Autowired
+	CalculadoraService service;
+	
+	 @LocalServerPort
+	 int puerto;
+
+	//Test 1: SUMAR
+	@Test
+	public void test1() throws URISyntaxException  {	
+		
+		RestTemplate template = new RestTemplate();
+	    String url = "http://localhost:" + puerto + "/api/calculadora?num1=" + 25 + "&num2=" + 63 + "&op=SUMAR" ;
+        URI uri = new URI(url);
+        ResponseEntity<BigDecimal> resultado = template.getForEntity(uri, BigDecimal.class);
+		assert(resultado != null );
+	}
+	
+	//Test 2: RESTAR
+	@Test
+	public void test2() throws InterruptedException, URISyntaxException {
+			RestTemplate template = new RestTemplate();
+			String url = "http://localhost:" + puerto + "/api/calculadora?num1=" + 90 + "&num2=" + 35 + "&op=RESTAR" ;
+	        URI uri = new URI(url);
+	        ResponseEntity<BigDecimal> resultado = template.getForEntity(uri, BigDecimal.class);
+			assert(resultado != null );
+			
+	}
+
+}
